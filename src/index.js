9a3671e1fd2a3c5c35ba10d5d5c7555b1c416373
toxAndreev <36535854+toxAndreev@users.noreply.github.com>
2020-02-26 17:01:43 +0200

    fix: change code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
+function my(x) {
+	return x.sort((a,b) => a-b);
+}
 exports.min = function min (array) {
-  return 0;
+
+	 return (typeof(array) == 'undefined' || array.length == 0)?0:my(array)[0];
+	
 }
 
 exports.max = function max (array) {
-  return 0;
+	
+	 return (array == undefined || array.length == 0)?0:my(array)[my(array).length-1];
+	
 }
 
 exports.avg = function avg (array) {
-  return 0;
+	
+	 return (array == undefined || array.length == 0)?0:my(array).reduce((sum, current) => sum + current, 0)/my(array).length;
+	
 }
